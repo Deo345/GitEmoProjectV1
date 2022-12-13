d9b543b72dd1755ed78a7bb03d91e05dc208ca45
anuj deo <anuj deo@anuj>
2022-12-13 16:10:36 +0530

    Add new code

--- src/Testjava/Test2.java
+++ src/Testjava/Test2.java
@@ -2,4 +2,19 @@ package Testjava;
 
 public class Test2 {
 
+	
+	public static void main(String[] args) {
+		
+		int a= 10;
+		
+		int b=20;
+		
+		System.out.println("print a"+a);
+		
+		
+		
+		
+	}
+	
+	
 }
